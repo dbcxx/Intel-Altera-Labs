1b9d216a83c6f6b90ef4b489cfda47174ae1ae45
dbcxx <40644301+dbcxx@users.noreply.github.com>
2019-04-10 12:50:59 -0400

    first commit

---
+++ lab1/part1/lab1part1.vhd
@@ -0,0 +1,16 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Lab1Part1 is
+	port
+	(
+		SW		: in std_logic_vector(9 downto 0);
+		LEDR	: out std_logic_vector(9 downto 0)
+	);
+end Lab1Part1;
+
+architecture Behavioral of Lab1Part1 is
+begin
+	LEDR <= SW;
+end Behavioral;
